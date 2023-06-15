d86f9b28d2a1605752a13b1a23dbf774cc9df44f
florienborg <flornbg014@gmail.com>
2023-06-15 19:08:41 +0200

    push para ver si se agrega carpeta components a main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import './App.css';
 import {Main} from './pages/Main';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Discover } from './pages/Discover';
 import { Join } from './pages/Join';
+import ButtonJoinNow from './components/buttonJoinNow.jsx';
 
 function App() {
   return ( 
@@ -14,8 +16,13 @@ function App() {
               <Route path='/' element={<Main />} />        
       </Routes>
     </Router>
+
+    <section>
+      <ButtonJoinNow/>
+    </section>
     </div>
   );
 }
+
 
 export default App;
---
+++ my-app/src/components/buttonJoinNow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./my-app/styleButtonJoin.css"
+
+const ButtonJoinNow = () => {
+  return (
+    <button>
+      Join Now
+    </button>
+  );
+};
+
+
+export default ButtonJoinNow
