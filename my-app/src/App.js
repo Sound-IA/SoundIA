9f3f6d06006de7cd05486a45992be7104069c37c
florienborg <flornbg014@gmail.com>
2023-06-15 13:02:17 +0200

    Creacion de carpetas

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {Main} from './pages/Main';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
+  return ( 
     <div className="App">
+      <Router>
+      <Routes>
+              {/* <Route path='/discover' element={<DiscoverPage/>} /> */}
+              <Route path='/' element={<Main/>} />
+    </Routes>
+      </Router>
+    
+  
+
+    
       
     </div>
   );
---
+++ my-app/src/pages/Main.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// function Main() {
+//   return (
+//     <div>Main</div>
+//   )
+// }
+
+// export default Main
+
+import React from 'react'
+
+export function Main() {
+  return (
+    <div>Main</div>
+  )
+}
