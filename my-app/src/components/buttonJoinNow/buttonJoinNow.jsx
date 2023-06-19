5444e5351feff079665e26fe85ee998d5b18c4af
florienborg <flornbg014@gmail.com>
2023-06-19 10:09:28 +0200

    Boton joinNow con Tailwind

--- my-app/src/components/buttonJoinNow/buttonJoinNow.jsx
+++ my-app/src/components/buttonJoinNow/buttonJoinNow.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./styleButtonJoin.css"
+// import "./styleButtonJoin.css"
 
 const ButtonJoinNow = () => {
   return (
-    <button className="ButtonJoinNow">
+    <button className="w-32 h-14 bg-[#1762A7] text-[#FFFFFF] text-base rounded-lg border-none cursor-pointer font-semibold">
       Join Now
     </button>
   );
