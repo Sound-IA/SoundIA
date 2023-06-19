faf199a63f625dfeed64b851828104cb440f2071
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-06-19 12:12:56 +0200

    form

--- my-app/src/pages/Join.jsx
+++ my-app/src/pages/Join.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
-import { MainJoin} from '../components/MainJoin'
-
-=======
 import { NavBar } from '../components/NavBar'
->>>>>>> dev
 
 export function Join() {
   return (
     <div>
-<<<<<<< HEAD
-      <MainJoin/>
-=======
       <NavBar />
->>>>>>> dev
     </div>
   )
 }
