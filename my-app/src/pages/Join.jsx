6b4f127b87a5d0d7c2ae7dfa410aab287c303e18
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-06-20 11:35:12 +0200

    mainjoin

--- my-app/src/pages/Join.jsx
+++ my-app/src/pages/Join.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavBar } from '../components/NavBar'
+import MainJoin from '../components/MainJoin'
 
 export function Join() {
   return (
     <div className='bg-general-background overflow-hidden	relative'>
       <NavBar />
+      <MainJoin/>
     </div>
   )
 }
