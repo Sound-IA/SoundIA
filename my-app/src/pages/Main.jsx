2471b2765a9e20e4fd74e74d77f6f292515c2c8c
florienborg <flornbg014@gmail.com>
2023-06-20 14:30:22 +0200

    añadir zindex a navbar de pagina Join

--- my-app/src/pages/Main.jsx
+++ my-app/src/pages/Main.jsx
@@ -11,10 +11,5 @@ export function Main() {
       <div className="bg-circle2-3  w-[6rem] h-[6rem] left-[7rem] bottom-[40rem] md:w-[22rem] md:h-[22rem] rounded-full absolute md:top-[6rem] md:left-[23rem]"></div>
       <div className="bg-circle2-3 w-[10rem] h-[10rem] left-80 top-[47rem]  md:w-[35rem] md:h-[35rem] rounded-full absolute md:top-[20rem] md:left-[65rem]"></div>
     </div>
-<<<<<<< HEAD
-  )
-}
-=======
   );
 }
->>>>>>> dev
