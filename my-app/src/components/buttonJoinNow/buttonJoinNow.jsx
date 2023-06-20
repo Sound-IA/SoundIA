99cf0fe75b87befad739355b98e6415f86e7202e
florienborg <flornbg014@gmail.com>
2023-06-20 12:42:24 +0200

    Agregué link de redireccion al boton Join Now del main

--- my-app/src/components/buttonJoinNow/buttonJoinNow.jsx
+++ my-app/src/components/buttonJoinNow/buttonJoinNow.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export function ButtonJoinNow() {
   return (
+    <Link to="/join">
     <button className="w-32 h-14 mt-6 bg-[#1762A7] text-[#FFFFFF] text-base rounded-lg border-none cursor-pointer font-semibold">
       Join Now
     </button>
+    </Link>
   );
 };
