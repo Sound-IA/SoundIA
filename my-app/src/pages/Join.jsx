d436d00a8c7f08fcb8d23eb5a4ecbb32d6b5525a
dkhurlop <denisekhurlopian@gmail.com>
2023-06-20 14:53:04 +0200

    responsive

--- my-app/src/pages/Join.jsx
+++ my-app/src/pages/Join.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../components/footer/footer";
-import { MainJoin } from "../components/MainJoin";
+import { MainJoin } from "../components/Form/MainJoin";
 import { NavBar } from "../components/NavBar";
 
 export function Join() {
@@ -9,7 +9,7 @@ export function Join() {
       <NavBar />
       <MainJoin />
       <Footer />
-      <div className="bg-circle1 w-[10rem] h-[10rem] rounded-full absolute top-[35rem] left-[-5rem] xl:w-[30rem] xl:h-[30rem] xl:top-[19rem] xl:left-[-5rem]"></div>
+      <div className="bg-circle1 w-[10rem] h-[10rem] rounded-full absolute top-[40rem] left-[-5rem] xl:w-[30rem] xl:h-[30rem] xl:top-[19rem] xl:left-[-5rem]"></div>
       <div className="bg-circle2-3 w-[10rem] h-[10rem] rounded-full absolute top-[-5rem] right-[10rem] xl:w-[30rem] xl:h-[30rem] xl:top-[-10rem] xl:right-[38rem]"></div>
     </div>
   );
