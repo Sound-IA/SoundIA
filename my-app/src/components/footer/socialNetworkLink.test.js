662b2ee7dc5be3455ead47545d81935239947d74
Sylvia Llorente <sylviall81@gmail.com>
2023-06-21 19:24:00 +0200

    test extra render social network

--- my-app/src/components/footer/socialNetworkLink.test.js
+++ my-app/src/components/footer/socialNetworkLink.test.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import TwitterLink from "./twitterLink";
 import FacebookLink from "./facebookLink";
 
 describe("TwitterLink", () => {
-  it("renders the Twitter logo", () => {
+  it("renders the Twitter logo and text", () => {
     const { getByAltText } = render(<TwitterLink />);
     const twitterLogo = getByAltText("Logo de la red social Twitter en color blanco a la derecha del pie de pagina de la web");
-
+    const twitterText = screen.getByText('Twitter')
+    
+    expect(twitterText).toBeInTheDocument();
     expect(twitterLogo).toBeInTheDocument();
     expect(twitterLogo.src).toContain("twitter.svg");
   });
@@ -17,7 +19,9 @@ describe("FacebookLink", () => {
     it("renders the Facebook logo", () => {
       const { getByAltText } = render(<FacebookLink />);
       const fbLogo = getByAltText("Logo de la red social Facebook en color blanco a la derecha del pie de pagina de la web");
-  
+      const fbText = screen.getByText('Facebook')
+    
+      expect(fbText).toBeInTheDocument();
       expect(fbLogo).toBeInTheDocument();
       expect(fbLogo.src).toContain("facebook.svg");
     });
