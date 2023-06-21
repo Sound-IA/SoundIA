f13bb1aafc327b9883fcd650d99b071c3b40cd63
Sylvia Llorente <sylviall81@gmail.com>
2023-06-21 20:43:40 +0200

    ultimo intento right margin

--- my-app/src/components/footer/facebookLink.jsx
+++ my-app/src/components/footer/facebookLink.jsx
@@ -3,7 +3,7 @@ import facebookLogo from "../../assets/images/facebook.svg";
 
 const FacebookLink = () => {
   return (
-    <div className = 'flex flex-col items-center mt-0.5 lg:flex-row lg:mx-12  xl:flex-row'>
+    <div className = 'flex flex-col items-center mt-0.5 lg:flex-row lg xl:flex-row'>
         <img className="w-4 mr-1"
           src={facebookLogo}
           alt="Logo de la red social Facebook en color blanco a la derecha del pie de pagina de la web"
--- my-app/src/components/footer/footer.jsx
+++ my-app/src/components/footer/footer.jsx
@@ -4,18 +4,19 @@ import FacebookLink from "./facebookLink";
 
 function Footer() {
   return (
-    <div className= "w-full font-semibold bg-footer-form-icon-bg text-white h-16 items-center md:w-full md:items-center lg:items-center lg:w-full lg:flex-row">
-      <div className="flex space-x-6 w-6/6 md:ml-46 lg:w-6/6 lg:justify-end lg:mr-10 lg:ml-60 lg:items-center xl:ml-60">
-        <ul className="flex ml-6 w-2/6 space-x-4 mr-4 lg:space-x-8 lg:w-2/6 md:space-x-8 xl:space-x-8">
-          <li ClassName="mr-2 lg:mr-8">About Us</li>
+    <div className="flex w-full font-semibold bg-footer-form-icon-bg text-white h-16 items-center md:w-full md:items-center lg:items-center lg:w-full lg:flex-row">
+      <div className="flex space-x-6 w-3/6 md:ml-4 lg:w-3/6 lg:items-center xl:ml-60">
+        <ul className="flex ml-6 w-2/6 space-x-4 mr-4 lg:w-2/6 md:space-x-8 xl:space-x-8">
+          <li className="mr-2">About Us</li>
           <li>Contact</li>
         </ul>
-        <div className=" w-3/6 flex items-end flex-row md:flex md:flex-row lg:w-4/6 md:ml-89 lg:items-end lg:flex lg:flex-row lg:items-center xl:flex xl:flex-row">
-        <TwitterLink />
-        <FacebookLink />
-        </div>
-    </div> 
       </div>
+      <div className=" w-3/6 flex items-end flex-row md:flex md:flex-row lg:w-3/6 lg:items-end lg:flex lg:flex-row lg:items-center xl:flex xl:flex-row">
+          <TwitterLink />
+          <FacebookLink />
+        </div>
+    </div>
   );
 }
+
 export default Footer;
