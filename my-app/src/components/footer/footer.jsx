a6b5b4745caae3fd62c2c0604358c26c0a6dc10d
Sylvia Llorente <sylviall81@gmail.com>
2023-06-21 16:44:08 +0200

    arreglos

--- my-app/src/components/footer/footer.jsx
+++ my-app/src/components/footer/footer.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import TwitterLink from "./twitterLink";
 import FacebookLink from "./facebookLink";
 
-
-function Footer () {
+function Footer() {
   return (
-    <div className = "flex w-180 font-semibold bg-footer-form-icon-bg text-white h-16 items-center lg:space-x-78 md:space-x-80 lg:flex-row lg:justify-between" >
-        <div className= "flex space-x-4 md:ml-46 lg:ml-60 xl:ml-60">
-          <ul className= "flex ml-6 w-48 space-x-2 mr-4 lg:space-x-8 md:space-x-8 xl:space-x-8" >
-          <li ClassName= "mr-2 lg:mr-8">About Us</li>
-          <li ClassName= "ml-4" >Contact</li>
-          </ul>
+    <div className="flex w-180 font-semibold bg-footer-form-icon-bg text-white h-16 items-center lg:space-x-78 md:space-x-80 lg:flex-row lg:justify-between">
+      <div className="flex space-x-4 md:ml-46 lg:ml-60 xl:ml-60">
+        <ul className="flex ml-6 w-48 space-x-2 mr-4 lg:space-x-8 md:space-x-8 xl:space-x-8">
+          <li ClassName="mr-2 lg:mr-8">About Us</li>
+          <li ClassName="ml-4">Contact</li>
+        </ul>
+        <TwitterLink />
+        <FacebookLink />
       </div>
-      <div className= "font-semibold flex w-48 md:space-x-8 lg:mx-20 lg:w-108 lg:space-x-2 xl:w-200 lg:flex-row lg:space-between lg: xl:mx-28">
-      <TwitterLink />
-      <FacebookLink />
-  
-    </div>
+     {/* <div className="font-semibold flex w-48 md:space-x-8 lg:mx-20 lg:w-108 lg:space-x-2 xl:w-200 lg:flex-row lg:space-between lg: xl:mx-28"></div>*/}
     </div>
   );
 }
-export default Footer
-
+export default Footer;
