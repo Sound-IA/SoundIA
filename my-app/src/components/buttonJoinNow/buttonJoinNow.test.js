ca4dc73ee6cb82962a6cf496dda2fc3edd47f55d
florienborg <flornbg014@gmail.com>
2023-06-21 09:23:12 +0200

    realizando test num2

--- my-app/src/components/buttonJoinNow/buttonJoinNow.test.js
+++ my-app/src/components/buttonJoinNow/buttonJoinNow.test.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { ButtonJoinNow } from './components/buttonJoinNow/buttonJoinNow';
+import { describe } from 'node:test';
 
 describe ('texto dentro de componente boton ButtonJoinNow', ()=> {
     test('boton contiene un string', () => {
@@ -9,3 +10,9 @@ describe ('texto dentro de componente boton ButtonJoinNow', ()=> {
     })
     });
     
+    describe('Click en botón de Join Now me redirige a página donde se encuentra el formulario', ()=> {
+      test ('botón redirige a pagina del formulario', () => { 
+      const { getByText } = render(<ButtonJoinNow/>)
+      const buttonJoinNow = getByText("/Join")
+      })
+    })
