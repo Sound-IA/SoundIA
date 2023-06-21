b9b44df54f7d532498e54aa1974fdfa4e4041a04
Sylvia Llorente <sylviall81@gmail.com>
2023-06-21 19:26:52 +0200

    redaccion lines 6 y 18 descripcion de test fichero socialNetworkLink.js

--- my-app/src/components/footer/socialNetworkLink.test.js
+++ my-app/src/components/footer/socialNetworkLink.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import TwitterLink from "./twitterLink";
 import FacebookLink from "./facebookLink";
 
-describe("TwitterLink", () => {
+describe("componente TwitterLink renderiza correctamente", () => {
   it("renders the Twitter logo and text", () => {
     const { getByAltText } = render(<TwitterLink />);
     const twitterLogo = getByAltText("Logo de la red social Twitter en color blanco a la derecha del pie de pagina de la web");
@@ -15,7 +15,7 @@ describe("TwitterLink", () => {
   });
 });
 
-describe("FacebookLink", () => {
+describe("componente FacebookLink renderiza correctamente", () => {
     it("renders the Facebook logo", () => {
       const { getByAltText } = render(<FacebookLink />);
       const fbLogo = getByAltText("Logo de la red social Facebook en color blanco a la derecha del pie de pagina de la web");
