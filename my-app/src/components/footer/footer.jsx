f37bd4a0abb5ac8399a30af3ba7922699e6a8310
Sylvia Llorente <sylviall81@gmail.com>
2023-06-22 16:31:27 +0200

    añadir z-index y position relative footer fichero footer.jsx linea 7

--- my-app/src/components/footer/footer.jsx
+++ my-app/src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import FacebookLink from "./facebookLink";
 
 function Footer() {
   return (
-    <div className="flex w-full font-semibold bg-footer-form-icon-bg text-white h-16 items-center md:w-full md:items-center lg:items-center lg:flex-row">
+    <div className="flex z-10 relative w-full font-semibold bg-footer-form-icon-bg text-white h-16 items-center md:w-full md:items-center lg:items-center lg:flex-row">
       <div className="flex w-6/6  space-x-6 xl:w-4/6 justify-between md:mx-auto  lg:items-center">
         <ul className="flex ml-8  space-x-1 mr-4  md:space-x-8 xl:space-x-8">
           <li className="mr-2">About Us</li>
